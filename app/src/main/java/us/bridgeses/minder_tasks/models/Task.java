fb27c1327090c6eb826bdcbf7e35f177b2e0ff27
Tony <tbridges42@gmail.com>
2016-04-13 19:48:01 -0500

    Make parcelable

--- app/src/main/java/us/bridgeses/minder_tasks/models/Task.java
+++ app/src/main/java/us/bridgeses/minder_tasks/models/Task.java
@@ -1,11 +1,14 @@
 package us.bridgeses.minder_tasks.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Calendar;
 
 /**
  * Basic model of a task. Immutable. Uses the builder pattern to handle multiple optional fields
  */
-public class Task {
+public class Task implements Parcelable {
 
     private String name;
     private long creationTime;
@@ -41,11 +44,66 @@ public class Task {
         return category;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeLong(creationTime);
+        dest.writeLong(dueTime);
+        dest.writeLong(duration);
+        dest.writeParcelable(category,0);
+    }
+
+    public static final Parcelable.Creator<Task> CREATOR =
+            new Parcelable.Creator<Task>() {
+
+                @Override
+                public Task createFromParcel(Parcel source) {
+                    Task.Builder builder = new Task.Builder(source.readString());
+                    builder.setCreationTime(source.readLong());
+                    builder.setDueTime(source.readLong());
+                    builder.setDuration(source.readLong());
+                    builder.setCategory((Category)
+                            source.readParcelable(Category.class.getClassLoader()));
+                    return builder.build();
+                }
+
+                @Override
+                public Task[] newArray(int size) {
+                    return new Task[size];
+                }
+            };
+
     public static class Builder {
         private String name;
         private long creationTime = Calendar.getInstance().getTimeInMillis();
         private long dueTime = -1L;
         private long duration = -1L;
+
+        public Category getCategory() {
+            return category;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public long getCreationTime() {
+            return creationTime;
+        }
+
+        public long getDueTime() {
+            return dueTime;
+        }
+
+        public long getDuration() {
+            return duration;
+        }
+
         private Category category = null;
 
         public Builder(String name) {
