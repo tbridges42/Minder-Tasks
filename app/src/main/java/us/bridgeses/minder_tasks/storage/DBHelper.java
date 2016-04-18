5b82ebe5f42755cdf57a422eb30e56d26527487e
Tony <tbridges42@gmail.com>
2016-04-17 19:18:33 -0500

    On upgrade recreate db

--- app/src/main/java/us/bridgeses/minder_tasks/storage/DBHelper.java
+++ app/src/main/java/us/bridgeses/minder_tasks/storage/DBHelper.java
@@ -25,6 +25,8 @@ public class DBHelper extends SQLiteOpenHelper implements TasksContract {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE " + CATEGORIES_TABLE);
+        db.execSQL("DROP TABLE " + TASKS_TABLE);
         onCreate(db);
     }
 }
