2eebf9908d294d8130204dc5c96a131b74ca11a6
Tony <tbridges42@gmail.com>
2016-04-17 20:22:53 -0500

    Complete task adapter

--- app/src/main/java/us/bridgeses/minder_tasks/adapters/TasksAdapter.java
+++ app/src/main/java/us/bridgeses/minder_tasks/adapters/TasksAdapter.java
@@ -2,6 +2,7 @@ package us.bridgeses.minder_tasks.adapters;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +12,12 @@ import android.widget.TextView;
 import java.util.Random;
 
 import us.bridgeses.minder_tasks.R;
+import us.bridgeses.minder_tasks.storage.TasksContract;
 
 /**
  * Created by Tony on 3/24/2016.
  */
-public class TasksAdapter extends CursorAdapter {
+public class TasksAdapter extends CursorAdapter implements TasksContract.TasksEntry {
 
     private LayoutInflater layoutInflater;
     private int rowLayout;
@@ -27,18 +29,24 @@ public class TasksAdapter extends CursorAdapter {
         layoutInflater = (LayoutInflater.from(context));
         this.rowLayout = rowLayout;
         this.badStuffs = badStuffs;
+        Log.d("taskAdapter", "Created task adapter");
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         View row = layoutInflater.inflate(rowLayout, parent, false);
-        TextView badStuff = (TextView) row.findViewById(R.id.bad_stuff);
-        badStuff.setText(badStuffs[new Random().nextInt(badStuffs.length)]);
+        row.setTag(badStuffs[new Random().nextInt(badStuffs.length)]);
         return row;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // TODO: populate row from cursor
+        TextView name = (TextView) view.findViewById(R.id.task);
+        TextView badStuff = (TextView) view.findViewById(R.id.bad_stuff);
+        name.setText(cursor.getString(cursor.getColumnIndex(COLUMN_NAME)));
+        if (view.getTag() != null) {
+            badStuff.setText((String)view.getTag());
+        }
+        Log.d("taskAdapter", "Bound view");
     }
 }
