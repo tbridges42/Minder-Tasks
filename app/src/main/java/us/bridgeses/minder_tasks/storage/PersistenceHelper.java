33915185b4ae905523dd59f76cc5859e5d641781
Tony <tbridges42@gmail.com>
2016-05-22 19:01:59 -0500

    Add persistence methods for category

--- app/src/main/java/us/bridgeses/minder_tasks/storage/PersistenceHelper.java
+++ app/src/main/java/us/bridgeses/minder_tasks/storage/PersistenceHelper.java
@@ -7,6 +7,7 @@ import android.content.res.Resources;
 import android.database.Cursor;
 import android.net.Uri;
 
+import us.bridgeses.minder_tasks.models.Category;
 import us.bridgeses.minder_tasks.models.Task;
 
 /**
@@ -17,15 +18,13 @@ import us.bridgeses.minder_tasks.models.Task;
 // TODO: break contentvalues -> task -> cursor into separate methods
 public class PersistenceHelper implements TasksContract {
 
-    // Stored context should be Application Context to avoid leaking activities and fragments
-    private final Context context;
+    private final ContentResolver resolver;
 
     public PersistenceHelper(Context context) {
-        this.context = context.getApplicationContext();
+        resolver = context.getContentResolver();
     }
 
     public long saveTask(Task task) {
-        final ContentResolver resolver = context.getContentResolver();
         final ContentValues values = new ContentValues();
         if (task.getId() >= 0) {
             values.put(TasksEntry._ID, task.getId());
@@ -45,19 +44,37 @@ public class PersistenceHelper implements TasksContract {
         return Long.parseLong(uri.getLastPathSegment());
     }
 
+    public long saveCategory(Category category) {
+        final ContentValues values = new ContentValues();
+        if (category.getId() >= 0) {
+            values.put(CategoryEntry._ID, category.getId());
+        }
+        values.put(CategoryEntry.COLUMN_NAME, category.getName());
+        values.put(CategoryEntry.COLUMN_COLOR, category.getColor());
+        Uri uri = resolver.insert(CategoryEntry.CATEGORY_URI, values);
+        if (uri == null) {
+            throw new Resources.NotFoundException("Failed to insert category");
+        }
+        return Long.parseLong(uri.getLastPathSegment());
+    }
+
     public Task loadTask(long id) throws Resources.NotFoundException {
-        final ContentResolver resolver = context.getContentResolver();
         Uri uri = TasksEntry.TASK_URI.buildUpon().appendPath(Long.toString(id)).build();
         final Cursor cursor = resolver.query(uri, null, null, null, null);
         if ((cursor == null) || (cursor.getCount() == 0)){
             throw new Resources.NotFoundException("Task not found");
         }
         cursor.moveToFirst();
-        String name = cursor.getString(cursor.getColumnIndex(TasksEntry.COLUMN_NAME));
-        long created_time = cursor.getLong(cursor.getColumnIndex(TasksEntry.COLUMN_CREATION_TIME));
-        int duration = cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_DURATION));
-        long due_time = cursor.getLong(cursor.getColumnIndex(TasksEntry.COLUMN_DUE_TIME));
-        boolean completed = cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_COMPLETED)) == 1;
+        final String name =
+                cursor.getString(cursor.getColumnIndex(TasksEntry.COLUMN_NAME));
+        final long created_time =
+                cursor.getLong(cursor.getColumnIndex(TasksEntry.COLUMN_CREATION_TIME));
+        final int duration =
+                cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_DURATION));
+        final long due_time =
+                cursor.getLong(cursor.getColumnIndex(TasksEntry.COLUMN_DUE_TIME));
+        final boolean completed =
+                cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_COMPLETED)) == 1;
         Task task = new Task.Builder(name)
                 .setId(id)
                 .setCreationTime(created_time)
@@ -69,8 +86,23 @@ public class PersistenceHelper implements TasksContract {
         return task;
     }
 
+    public Category loadCategory(long id) throws Resources.NotFoundException {
+        Uri uri = CategoryEntry.CATEGORY_URI.buildUpon().appendPath(Long.toString(id)).build();
+        final Cursor cursor = resolver.query(uri, null, null, null, null);
+        if ((cursor == null) || (cursor.getCount() == 0)){
+            throw new Resources.NotFoundException("Category not found");
+        }
+        cursor.moveToFirst();
+        final String name =
+                cursor.getString(cursor.getColumnIndex(CategoryEntry.COLUMN_NAME));
+        final int color =
+                cursor.getInt(cursor.getColumnIndex(CategoryEntry.COLUMN_COLOR));
+        Category category = new Category(id, name, color);
+        cursor.close();
+        return category;
+    }
+
     public void recordCompletedTask(long id) {
-        final ContentResolver resolver = context.getContentResolver();
         final ContentValues values = new ContentValues();
         values.put(TasksEntry.COLUMN_COMPLETED, 1);
         resolver.update(TasksEntry.TASK_URI, values, TasksEntry._ID + " = ?",
@@ -78,8 +110,12 @@ public class PersistenceHelper implements TasksContract {
     }
 
     public void deleteTask(long id) {
-        final ContentResolver resolver = context.getContentResolver();
         resolver.delete(TasksEntry.TASK_URI, TasksEntry._ID + " = ?",
+                new String[] { Long.toString(id) });
+    }
+
+    public void deleteCategory(long id) {
+        resolver.delete(CategoryEntry.CATEGORY_URI, CategoryEntry._ID + " = ?",
                 new String[] { Long.toString(id) });
     }
 }
