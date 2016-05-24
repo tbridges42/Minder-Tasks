81e483460539e088179bead6e1e920272ef36f1a
Tony <tbridges42@gmail.com>
2016-05-23 22:01:10 -0500

    General code cleanup

--- app/src/main/java/us/bridgeses/minder_tasks/adapters/CursorRecyclerViewAdapter.java
+++ app/src/main/java/us/bridgeses/minder_tasks/adapters/CursorRecyclerViewAdapter.java
@@ -1,41 +1,51 @@
-package us.bridgeses.minder_tasks.adapters;
 /*
- * Copyright (C) 2014 skyfish.jy@gmail.com
+ * Copyright 2016 Tony Bridges
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
-import android.app.Activity;
-import android.content.Context;
-import android.database.ContentObserver;
+package us.bridgeses.minder_tasks.adapters;
+
+/*
+ * Copyright 2016 Tony Bridges
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import android.database.Cursor;
 import android.database.DataSetObserver;
-import android.net.Uri;
-import android.os.Handler;
-import android.os.Looper;
+import android.provider.BaseColumns;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 /**
- * Created by skyfishjy on 10/31/14.
- *
  * Modified by Tony on 4/22/16
  *
  * This is a view adapter for recycler views backed by a cursor.
  */
 public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHolder>
         extends RecyclerView.Adapter<VH> implements Swappable {
+
+    public static final int INVALID_ID = -1;
 
     private Cursor mCursor;
 
@@ -48,10 +58,9 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
     public CursorRecyclerViewAdapter(Cursor cursor) {
         mCursor = cursor;
         mDataValid = cursor != null;
-        mRowIdColumn = mDataValid ? mCursor.getColumnIndex("_id") : -1;
+        mRowIdColumn = mDataValid ? mCursor.getColumnIndex(BaseColumns._ID) : -1;
         mDataSetObserver = new NotifyingDataSetObserver();
         if (mCursor != null) {
-            Log.d("cursor","Setting observer");
             mCursor.registerDataSetObserver(mDataSetObserver);
         }
     }
@@ -76,11 +85,40 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
         return 0;
     }
 
+    /**
+     * Returns the position within the cursor of the selected element
+     * @param id The id of the element to be found
+     * @return the 0 based position of the element, or {@link #INVALID_ID} if the element is not found
+     */
+    public int getPosition(long id) {
+        int currPos = mCursor.getPosition();
+        int index = 0;
+        mCursor.moveToFirst();
+        do {
+            if (mCursor.getInt(mCursor.getColumnIndex(BaseColumns._ID)) == id) {
+                break;
+            }
+            index++;
+            if (!mCursor.moveToNext()) {
+                index = INVALID_ID;
+            }
+        } while (true);
+        mCursor.moveToPosition(currPos);
+        return index;
+    }
+
     @Override
     public void setHasStableIds(boolean hasStableIds) {
         super.setHasStableIds(true);
     }
 
+    /**
+     * Binds the data within the cursor to the view in the viewholder. The cursor is already moved
+     * to the correct position.
+     * {@see android.support.v7.widget.RecyclerView#onBindViewHolder(ViewHolder, int)}
+     * @param viewHolder Holds the view to be bound
+     * @param cursor Holds the data to be bound
+     */
     public abstract void onBindViewHolder(VH viewHolder, Cursor cursor);
 
     @Override
@@ -123,7 +161,7 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
             if (mDataSetObserver != null) {
                 mCursor.registerDataSetObserver(mDataSetObserver);
             }
-            mRowIdColumn = newCursor.getColumnIndexOrThrow("_id");
+            mRowIdColumn = newCursor.getColumnIndexOrThrow(BaseColumns._ID);
             mDataValid = true;
             notifyDataSetChanged();
         } else {
@@ -139,7 +177,6 @@ public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHold
         @Override
         public void onChanged() {
             super.onChanged();
-            Log.d("NotifyingDataSetObs", "Update observed");
             mDataValid = true;
             notifyDataSetChanged();
         }
