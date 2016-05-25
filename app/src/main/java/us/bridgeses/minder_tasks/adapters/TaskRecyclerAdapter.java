414b829b39e4c0038e80eecd826b64bdae3a34c5
Tony <tbridges42@gmail.com>
2016-05-24 20:14:27 -0500

    Implement theming

--- app/src/main/java/us/bridgeses/minder_tasks/adapters/TaskRecyclerAdapter.java
+++ app/src/main/java/us/bridgeses/minder_tasks/adapters/TaskRecyclerAdapter.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright 2016 Tony Bridges
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package us.bridgeses.minder_tasks.adapters;
 
-import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +27,15 @@ import android.widget.TextView;
 import java.util.Random;
 
 import us.bridgeses.minder_tasks.R;
+import us.bridgeses.minder_tasks.interfaces.Swappable;
 import us.bridgeses.minder_tasks.storage.TasksContract;
+import us.bridgeses.minder_tasks.theme.DefaultTheme;
+import us.bridgeses.minder_tasks.theme.Theme;
 
 /**
  * Created by Tony on 4/17/2016.
  *
- * This is a concrete @see CursorRecyclerViewAdapter for working with Tasks
+ * This is a concrete {@link CursorRecyclerViewAdapter} for working with Tasks
  */
 
 // TODO: How can this be generalized?
@@ -27,7 +44,7 @@ public class TaskRecyclerAdapter
         implements TasksContract, Swappable {
 
     /**
-     * This @see ViewHolder holds the views in the row layout and binds listeners to it
+     * This {@link ViewHolder} holds the views in the row layout and binds listeners to it
      */
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
             View.OnLongClickListener {
@@ -35,12 +52,12 @@ public class TaskRecyclerAdapter
         public final View itemView;
         public final TextView name;
         public final TextView badStuff;
-        public final String badStuffString;
+        public final int badStuffIndex;
         public final TaskListener listener;
         // TODO: Add checkmark
 
 
-        public ViewHolder(View itemView, String badStuffString, TaskListener listener) {
+        public ViewHolder(View itemView, int badStuffIndex, TaskListener listener) {
             super(itemView);
             this.itemView = itemView;
             itemView.setOnClickListener(this);
@@ -48,7 +65,7 @@ public class TaskRecyclerAdapter
             itemView.findViewById(R.id.complete).setOnClickListener(this);
             name = (TextView) itemView.findViewById(R.id.task);
             badStuff = (TextView) itemView.findViewById(R.id.bad_stuff);
-            this.badStuffString = badStuffString;
+            this.badStuffIndex = badStuffIndex;
             this.listener = listener;
         }
 
@@ -81,40 +98,57 @@ public class TaskRecyclerAdapter
         void onItemComplete(long id, View v);
     }
 
-    private final String[] badStuff;
     private final TaskListener listener;
+    private Theme mTheme = new DefaultTheme();
 
-    public TaskRecyclerAdapter (Cursor c, String[] badStuff,
-                                TaskListener listener) {
+    public TaskRecyclerAdapter (Cursor c, TaskListener listener) {
+        this(c, listener, null);
+    }
+
+    public TaskRecyclerAdapter (Cursor c,
+                                TaskListener listener, Theme theme) {
         super(c);
-        this.badStuff = badStuff;
         this.listener = listener;
+        if (theme != null) {
+            mTheme = theme;
+        }
     }
 
     @Override
     public TaskRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
+        final View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.default_row, parent, false);
-        ViewHolder vh = new ViewHolder(view, badStuff[new Random().nextInt(98)+1], listener);
-        return vh;
+        int index;
+        if ((mTheme.getAllBadStuff() == null) || (mTheme.getAllBadStuff().length == 0)) {
+            index = INVALID_ID;
+        }
+        else {
+            index = new Random().nextInt(mTheme.getAllBadStuff().length - 1);
+        }
+        return new ViewHolder(view, index, listener);
     }
+
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, Cursor cursor) {
-        Log.d("TaskRecyclerAdapter", cursor.getString(cursor.getColumnIndex(TasksEntry.COLUMN_NAME)));
-        viewHolder.badStuff.setText(viewHolder.badStuffString);
+        if (viewHolder.badStuffIndex != INVALID_ID) {
+            viewHolder.badStuff.setText(mTheme.getBadStuff(viewHolder.badStuffIndex));
+        }
+        else {
+            viewHolder.badStuff.setText("");
+        }
+        viewHolder.badStuff.setTextColor(mTheme.getPrimaryFontColor());
         viewHolder.name.setText(cursor.getString(cursor.getColumnIndex(TasksEntry.COLUMN_NAME)));
+        viewHolder.name.setTextColor(mTheme.getPrimaryFontColor());
         if (cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_COMPLETED)) == 1) {
             // Grey out completed task
             viewHolder.itemView.setAlpha(0.8f);
             viewHolder.itemView.setBackgroundColor(Color.GRAY);
-
         }
         else {
             // Ungrey task if not completed
             viewHolder.itemView.setAlpha(1f);
-            // TODO: How to set this to layout default?
-            viewHolder.itemView.setBackgroundColor(Color.WHITE);
+            viewHolder.itemView.setBackgroundColor(mTheme.getPrimaryColor());
         }
     }
 }
