0067f356c8be74112161a2a29802d2f4782a5a0e
Tony <tbridges42@gmail.com>
2016-05-31 20:28:59 -0500

    General code improvements and add basic theming

--- app/src/main/java/us/bridgeses/minder_tasks/fragments/CategoryEditorFragment.java
+++ app/src/main/java/us/bridgeses/minder_tasks/fragments/CategoryEditorFragment.java
@@ -1,36 +1,67 @@
+/*
+ * Copyright 2016 Tony Bridges
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package us.bridgeses.minder_tasks.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import petrov.kristiyan.colorpicker.ColorPicker;
 import us.bridgeses.minder_tasks.R;
+import us.bridgeses.minder_tasks.interfaces.Themeable;
 import us.bridgeses.minder_tasks.models.Category;
 import us.bridgeses.minder_tasks.storage.PersistenceHelper;
+import us.bridgeses.minder_tasks.theme.DefaultTheme;
+import us.bridgeses.minder_tasks.theme.Theme;
 
 /**
- * Created by tbrid on 5/22/2016.
+ * Fragment for creating and modifying categories.
  */
-public class CategoryEditorFragment extends DialogFragment implements View.OnClickListener, ColorPicker.OnFastChooseColorListener {
+public class CategoryEditorFragment extends DialogFragment
+        implements View.OnClickListener, ColorPicker.OnFastChooseColorListener, Themeable {
+
+    private static final String CATEGORY_TAG = "category";
 
     private Category category;
     private EditText nameInput;
     private LinearLayout colorBlock;
     private SaveListener listener;
+    private Theme mTheme = new DefaultTheme();
+
+    @Override
+    public void applyTheme(@NonNull Theme theme) {
+        if (mTheme == theme) {
+            return;
+        }
+        mTheme = theme;
+        // TODO: Apply theme
+    }
 
     public interface SaveListener {
         void handleSave(long id);
@@ -40,7 +71,7 @@ public class CategoryEditorFragment extends DialogFragment implements View.OnCli
         CategoryEditorFragment frag = new CategoryEditorFragment();
 
         Bundle args  = new Bundle();
-        args.putParcelable("category", category);
+        args.putParcelable(CATEGORY_TAG, category);
         frag.setArguments(args);
 
         return frag;
@@ -55,16 +86,9 @@ public class CategoryEditorFragment extends DialogFragment implements View.OnCli
         super.onCreate(savedInstanceState);
         Bundle args = getArguments();
         if ((args != null) && (!args.isEmpty())) {
-            category = args.getParcelable("category");
+            category = args.getParcelable(CATEGORY_TAG);
         }
     }
-
-    /*@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.category_editor_layout, parent, false);
-
-        return v;
-    }*/
 
     @Override
     @NonNull
@@ -85,7 +109,10 @@ public class CategoryEditorFragment extends DialogFragment implements View.OnCli
                 })
                 .setTitle("Title");
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.category_editor_layout, null);
+        v.setBackgroundColor(mTheme.getPrimaryColor());
         nameInput = (EditText) v.findViewById(R.id.input_category_name);
+        nameInput.setTextColor(mTheme.getPrimaryFontColor());
+        nameInput.getBackground().setColorFilter(mTheme.getHighlightColor(), PorterDuff.Mode.SRC_IN);
         colorBlock = (LinearLayout) v.findViewById(R.id.color_selector_block);
         colorBlock.setOnClickListener(this);
         if (category != null) {
@@ -98,6 +125,31 @@ public class CategoryEditorFragment extends DialogFragment implements View.OnCli
         }
         dialogBuilder.setView(v);
         return dialogBuilder.create();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        applyTheme();
+    }
+
+    private void applyTheme() {
+        Dialog dialog = getDialog();
+        LinearLayout titleLayout = (LinearLayout) dialog.findViewById(
+                dialog.getContext().getResources()
+                        .getIdentifier("android:id/topPanel", null, null));
+        TextView title = (TextView) dialog.findViewById(
+                dialog.getContext().getResources()
+                        .getIdentifier("android:id/alertTitle", null, null));
+        View bar = dialog.findViewById(dialog.getContext().getResources()
+                .getIdentifier("android:id/titleDivider", null, null));
+        Button ok = (Button) dialog.findViewById(dialog.getContext().getResources()
+                .getIdentifier("android:id/button1", null, null));
+        title.setTextColor(mTheme.getPrimaryFontColor());
+        titleLayout.getBackground().setColorFilter(mTheme.getPrimaryColor(), PorterDuff.Mode.MULTIPLY);
+        bar.setBackgroundColor(mTheme.getHighlightColor());
+        ok.setBackgroundColor(mTheme.getPrimaryColor());
+        ok.setTextColor(mTheme.getPrimaryFontColor());
     }
 
     public void save() {
