7484c58b5945a0f6c7a7551cd94e3eb1da1b61dc
Tony <tbridges42@gmail.com>
2016-05-31 22:35:07 -0500

    General code improvements

--- app/src/main/java/us/bridgeses/minder_tasks/fragments/TaskEditorFragment.java
+++ app/src/main/java/us/bridgeses/minder_tasks/fragments/TaskEditorFragment.java
@@ -1,9 +1,7 @@
 package us.bridgeses.minder_tasks.fragments;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -15,19 +13,14 @@ import android.support.v4.widget.CursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import java.util.Date;
 
 import us.bridgeses.dateview.DateView;
 import us.bridgeses.minder_tasks.R;
 import us.bridgeses.minder_tasks.adapters.CategorySpinnerAdapter;
-import us.bridgeses.minder_tasks.adapters.CursorAdapterWithNew;
 import us.bridgeses.minder_tasks.models.Category;
 import us.bridgeses.minder_tasks.models.Task;
 import us.bridgeses.minder_tasks.storage.PersistenceHelper;
@@ -42,45 +35,45 @@ public class TaskEditorFragment extends DialogFragment
                             implements View.OnClickListener,
                             CategoryEditorFragment.SaveListener {
 
+    private static final String CAT_DIALOG_TAG = "cat_dialog_tag";
+
     private Task.Builder taskBuilder;
     private EditText inputTitle;
     private EditText inputDuration;
     private Spinner categorySpinner;
     private DateView inputTime;
-    private Button save;
-    private Button cancel;
-    private CloseListener callback;
 
     @Override
     public void onClick(View v) {
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (v.getTag() == "date") {
+            editDate();
+        }
+        else {
+            showCategoryDialog();
+        }
+    }
+
+    private void showCategoryDialog() {
+        final FragmentTransaction ft = getFragmentManager().beginTransaction();
+        final Fragment prev = getFragmentManager().findFragmentByTag(CAT_DIALOG_TAG);
         if (prev != null) {
             ft.remove(prev);
         }
         ft.addToBackStack(null);
 
         // Create and show the dialog.
-        CategoryEditorFragment newFragment = CategoryEditorFragment.newInstance(null);
+        final CategoryEditorFragment newFragment = CategoryEditorFragment.newInstance(null);
         newFragment.setListener(this);
-        newFragment.show(ft, "dialog");
+        newFragment.show(ft, CAT_DIALOG_TAG);
     }
 
     @Override
     public void handleSave(long id) {
-        CursorAdapter adapter = (CursorAdapter) categorySpinner.getAdapter();
-        final Cursor newCursor = getContext().getContentResolver().query(TasksContract.CategoryEntry.CATEGORY_URI,
-                new String[] { TasksContract.CategoryEntry._ID,
-                        TasksContract.CategoryEntry.COLUMN_NAME,
-                        TasksContract.CategoryEntry.COLUMN_COLOR },
-                null, null, null);
+        final CursorAdapter adapter = (CursorAdapter) categorySpinner.getAdapter();
+        final Cursor newCursor = new PersistenceHelper(getContext()).loadAllCategories();
         adapter.swapCursor(newCursor);
         // ID counts from 1, but selection counts from 0
         categorySpinner.setSelection((int)id - 1);
-    }
-
-    public interface CloseListener {
-        void close();
     }
 
     public static TaskEditorFragment newInstance(Task task) {
@@ -105,36 +98,6 @@ public class TaskEditorFragment extends DialogFragment
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        try {
-            callback = (CloseListener) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " must implement CloseListener");
-        }
-    }
-
-    @SuppressWarnings("deprecation") // This method is necessary for SDK < 23
-    @Override
-    public void onAttach(Activity context) {
-        super.onAttach(context);
-        try {
-            callback = (CloseListener) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " must implement CloseListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        callback = null;
-        super.onDetach();
-    }
-
-
-    @Override
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity())
@@ -142,6 +105,7 @@ public class TaskEditorFragment extends DialogFragment
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         save();
+                        dialog.dismiss();
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -174,17 +138,14 @@ public class TaskEditorFragment extends DialogFragment
         inputTitle.setText(taskBuilder.getName());
         inputDuration.setText(taskBuilder.getDuration() + "");
         inputTime.setDate(taskBuilder.getDueTime());
+        inputTime.setTag("date");
         inputTime.setClickable(true);
-        inputTime.setOnClickListener(new dateListener());
+        inputTime.setOnClickListener(this);
     }
 
     private void initSpinner(Spinner spinner) {
         CursorAdapter cAdapter = new CategorySpinnerAdapter(getContext(),
-                getContext().getContentResolver().query(TasksContract.CategoryEntry.CATEGORY_URI,
-                        new String[] { TasksContract.CategoryEntry._ID,
-                                TasksContract.CategoryEntry.COLUMN_NAME,
-                                TasksContract.CategoryEntry.COLUMN_COLOR },
-                        null, null, null),
+                new PersistenceHelper(getContext()).loadAllCategories(),
                 0, this);
         spinner.setAdapter(cAdapter);
         if (taskBuilder.getCategory() == null) {
@@ -197,7 +158,7 @@ public class TaskEditorFragment extends DialogFragment
         }
     }
 
-    public void save() {
+    private void save() {
         PersistenceHelper helper = new PersistenceHelper(getActivity());
         long id = categorySpinner.getSelectedItemId();
         if (id != Spinner.INVALID_ROW_ID) {
@@ -206,46 +167,17 @@ public class TaskEditorFragment extends DialogFragment
         }
         taskBuilder.setName(inputTitle.getText().toString());
         helper.saveTask(taskBuilder.build());
-        callback.close();
     }
 
-    public void cancel() {
-        callback.close();
-    }
-
-    public void editDate() {
+    private void editDate() {
         SlideDateTimePicker.Builder dateTimePickerBuilder = new SlideDateTimePicker.Builder(
                 getActivity().getSupportFragmentManager()
         );
         dateTimePickerBuilder.setListener(new SetDateTimeListener());
-        if (taskBuilder.getDueTime() > 0) {
+        if (taskBuilder.getDueTime() != -1) {
             dateTimePickerBuilder.setInitialDate(new Date(taskBuilder.getDueTime()));
         }
         dateTimePickerBuilder.build().show();
-    }
-
-    private class saveListener implements Button.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            save();
-        }
-    }
-
-    private class cancelListener implements Button.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            cancel();
-        }
-    }
-
-    private class dateListener implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            editDate();
-        }
     }
 
     private class SetDateTimeListener extends SlideDateTimeListener {
