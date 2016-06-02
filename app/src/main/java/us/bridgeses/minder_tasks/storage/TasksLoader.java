454759bf9599dc3e554c611033de562669004b59
Tony <tbridges42@gmail.com>
2016-06-01 20:29:14 -0500

    General code improvements

--- app/src/main/java/us/bridgeses/minder_tasks/storage/TasksLoader.java
+++ app/src/main/java/us/bridgeses/minder_tasks/storage/TasksLoader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Tony Bridges
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package us.bridgeses.minder_tasks.storage;
 
 import android.content.Context;
@@ -7,7 +23,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 
-import us.bridgeses.minder_tasks.adapters.Swappable;
+import us.bridgeses.minder_tasks.interfaces.Swappable;
 
 /**
  * Created by Tony on 4/23/2016.
@@ -37,6 +53,7 @@ public class TasksLoader extends SwappableLoader implements TasksContract.TasksE
             select = "( " + COLUMN_CATEGORY + "= ? )";
             selectArgs = new String[] {Integer.toString(categoryId)};
         }
+        // Sort first incomplete followed by complete, then selected sort
         final String sort = COLUMN_COMPLETED + " ASC, " +
                 sortColumn + " " + sortOrder;
         return new CursorLoader(context, uri, SUMMARY_PROJECTION, select, selectArgs, sort);
