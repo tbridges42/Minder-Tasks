013d5c2228a71ad259131097e4c8e2666ea3ba97
Tony <tbridges42@gmail.com>
2016-06-01 20:32:08 -0500

    General code improvements

--- app/src/main/java/us/bridgeses/minder_tasks/theme/Theme.java
+++ app/src/main/java/us/bridgeses/minder_tasks/theme/Theme.java
@@ -1,11 +1,25 @@
+/*
+ * Copyright 2016 Tony Bridges
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package us.bridgeses.minder_tasks.theme;
 
-import java.util.List;
-
-import us.bridgeses.minder_tasks.R;
+import android.text.Spanned;
 
 /**
- * Created by Tony on 12/19/2015.
+ * An interface for a theme that will provide all customizable resources.
  */
 public abstract class Theme {
     public abstract int getLogo();
@@ -16,6 +30,6 @@ public abstract class Theme {
     public abstract int getTextSize();
     public abstract int getHeadlineSize();
     public abstract int getSmallTextSize();
-    public abstract String[] getAllBadStuff();
-    public abstract String getBadStuff();
+    public abstract Spanned[] getAllBadStuff();
+    public abstract Spanned getBadStuff(int index);
 }
