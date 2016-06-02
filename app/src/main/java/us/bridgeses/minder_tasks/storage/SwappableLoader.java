4a476efbfa63db87b735fcc7db805bfbb6a95e60
Tony <tbridges42@gmail.com>
2016-06-01 20:14:29 -0500

    General code improvements

--- app/src/main/java/us/bridgeses/minder_tasks/storage/SwappableLoader.java
+++ app/src/main/java/us/bridgeses/minder_tasks/storage/SwappableLoader.java
@@ -1,16 +1,28 @@
+/*
+ * Copyright 2016 Tony Bridges
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package us.bridgeses.minder_tasks.storage;
 
 import android.app.LoaderManager;
 import android.content.Context;
-import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
-import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
-import us.bridgeses.minder_tasks.adapters.CursorRecyclerViewAdapter;
-import us.bridgeses.minder_tasks.adapters.Swappable;
+import us.bridgeses.minder_tasks.interfaces.Swappable;
 
 /**
  * Created by Tony on 4/23/2016.
@@ -23,10 +35,6 @@ public abstract class SwappableLoader implements LoaderManager.LoaderCallbacks<C
     // Only store application context to prevent leaks of activities or fragments
     protected final Context context;
 
-    // Does this belong here?
-    public static final int TASK_LOADER = 0;
-    public static final int CATEGORY_LOADER = 1;
-
     public SwappableLoader(Context context, Swappable adapter) {
         this.context = context.getApplicationContext();
         this.adapter = adapter;
@@ -34,7 +42,6 @@ public abstract class SwappableLoader implements LoaderManager.LoaderCallbacks<C
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        Log.d("Swaping cursor", data.getCount() + " Records");
         adapter.swapCursor(data);
     }
 
