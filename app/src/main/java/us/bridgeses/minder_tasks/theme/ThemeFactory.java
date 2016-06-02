98b520a0f7680a5b0b0765da0b5bf7420093904c
Tony <tbridges42@gmail.com>
2016-06-01 20:33:53 -0500

    General code improvements

--- app/src/main/java/us/bridgeses/minder_tasks/theme/ThemeFactory.java
+++ app/src/main/java/us/bridgeses/minder_tasks/theme/ThemeFactory.java
@@ -1,8 +1,25 @@
+/*
+ * Copyright 2016 Tony Bridges
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package us.bridgeses.minder_tasks.theme;
 
 /**
- * Created by Tony on 12/19/2015.
+ * Return a theme by name.
  */
+// TODO: Should themes be selected by name?
 public class ThemeFactory {
 
     private final String theme;
@@ -11,7 +28,8 @@ public class ThemeFactory {
         this.theme = theme;
     }
 
+    // TODO: Add more themes
     public Theme getTheme() {
-        return null;
+        return new DefaultTheme();
     }
 }
