8dd2808235723d892ecdf0557dc329694946263e
Tony <tbridges42@gmail.com>
2016-06-24 23:46:06 -0500

    Extract interface

--- app/src/main/java/us/bridgeses/minder_tasks/storage/PersistenceHelper.java
+++ app/src/main/java/us/bridgeses/minder_tasks/storage/PersistenceHelper.java
@@ -1,8 +1,21 @@
+/*
+ * Copyright 2016 Tony Bridges
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package us.bridgeses.minder_tasks.storage;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.net.Uri;
@@ -11,154 +24,25 @@ import us.bridgeses.minder_tasks.models.Category;
 import us.bridgeses.minder_tasks.models.Task;
 
 /**
- * Created by Tony on 4/16/2016.
- *
- * Helper for persisting and retrieving tasks and categories
+ * Created by tbrid on 6/24/2016.
  */
-public class PersistenceHelper implements TasksContract {
+public interface PersistenceHelper {
+    Cursor getRecords(Uri uri, String[] projection, String selection,
+                      String[] selectionArgs, String orderBy);
 
-    private final ContentResolver resolver;
+    long saveTask(Task task);
 
-    public PersistenceHelper(Context context) {
-        resolver = context.getContentResolver();
-    }
+    long saveCategory(Category category);
 
-    public Cursor getRecords(Uri uri, String[] projection, String selection,
-                             String[] selectionArgs, String orderBy) {
-        return resolver.query(uri, projection, selection, selectionArgs, orderBy);
-    }
+    Task loadTask(long id) throws Resources.NotFoundException;
 
-    /**
-     * Convert a Cursor row to a Task
-     * @param cursor the cursor must already be set to the correct row
-     */
-    public Task taskFromCursor(Cursor cursor) {
-        final long id =
-                cursor.getLong(cursor.getColumnIndex(TasksEntry._ID));
-        final Category category =
-                loadCategory(cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_CATEGORY)));
-        final String name =
-                cursor.getString(cursor.getColumnIndex(TasksEntry.COLUMN_NAME));
-        final long created_time =
-                cursor.getLong(cursor.getColumnIndex(TasksEntry.COLUMN_CREATION_TIME));
-        final int duration =
-                cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_DURATION));
-        final long due_time =
-                cursor.getLong(cursor.getColumnIndex(TasksEntry.COLUMN_DUE_TIME));
-        final boolean completed =
-                cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_COMPLETED)) == 1;
-        return new Task.Builder(name)
-                .setId(id)
-                .setCategory(category)
-                .setCreationTime(created_time)
-                .setDueTime(due_time)
-                .setDuration(duration)
-                .setCompleted(completed)
-                .build();
-    }
+    Category loadCategory(long id) throws Resources.NotFoundException;
 
-    public Category categoryFromCursor(Cursor cursor) {
-        final Long id =
-                cursor.getLong(cursor.getColumnIndex(CategoryEntry._ID));
-        final String name =
-                cursor.getString(cursor.getColumnIndex(CategoryEntry.COLUMN_NAME));
-        final int color =
-                cursor.getInt(cursor.getColumnIndex(CategoryEntry.COLUMN_COLOR));
-        return new Category(id, name, color);
-    }
+    Cursor loadAllCategories();
 
-    public ContentValues cvFromTask(Task task) {
-        final ContentValues values = new ContentValues();
-        if (task.getId() >= 0) {
-            values.put(TasksEntry._ID, task.getId());
-        }
-        values.put(TasksEntry.COLUMN_NAME, task.getName());
-        if (task.getCategory() != null) {
-            values.put(TasksEntry.COLUMN_CATEGORY, task.getCategory().getId());
-        }
-        values.put(TasksEntry.COLUMN_CREATION_TIME, task.getCreationTime());
-        values.put(TasksEntry.COLUMN_DURATION, task.getDuration());
-        values.put(TasksEntry.COLUMN_DUE_TIME, task.getDueTime());
-        values.put(TasksEntry.COLUMN_COMPLETED, task.isCompleted());
-        return values;
-    }
+    void recordCompletedTask(long id);
 
-    public ContentValues cvFromCategory(Category category) {
-        final ContentValues values = new ContentValues();
-        if (category.getId() >= 0) {
-            values.put(CategoryEntry._ID, category.getId());
-        }
-        values.put(CategoryEntry.COLUMN_NAME, category.getName());
-        values.put(CategoryEntry.COLUMN_COLOR, category.getColor());
-        return values;
-    }
+    void deleteTask(long id);
 
-    public long saveTask(Task task) {
-        final ContentValues values = cvFromTask(task);
-        Uri uri = resolver.insert(TasksEntry.TASK_URI, values);
-        if (uri == null) {
-            throw new Resources.NotFoundException("Failed to insert task");
-        }
-        return Long.parseLong(uri.getLastPathSegment());
-    }
-
-    public long saveCategory(Category category) {
-        final ContentValues values = cvFromCategory(category);
-        Uri uri = resolver.insert(CategoryEntry.CATEGORY_URI, values);
-        if (uri == null) {
-            throw new Resources.NotFoundException("Failed to insert category");
-        }
-        return Long.parseLong(uri.getLastPathSegment());
-    }
-
-    public Task loadTask(long id) throws Resources.NotFoundException {
-        Uri uri = TasksEntry.TASK_URI.buildUpon().appendPath(Long.toString(id)).build();
-        final Cursor cursor = getRecords(uri, null, null, null, null);
-        if ((cursor == null) || (cursor.getCount() == 0)){
-            throw new Resources.NotFoundException("Task not found");
-        }
-        cursor.moveToFirst();
-        Task task = taskFromCursor(cursor);
-        cursor.close();
-        return task;
-    }
-
-    private Cursor loadAllTasks() {
-        Uri uri = TasksEntry.TASK_URI;
-        return resolver.query(uri, TasksEntry.SUMMARY_PROJECTION, null, null, null);
-    }
-
-    public Category loadCategory(long id) throws Resources.NotFoundException {
-        Uri uri = CategoryEntry.CATEGORY_URI.buildUpon().appendPath(Long.toString(id)).build();
-        final Cursor cursor = getRecords(uri, null, null, null, null);
-        if ((cursor == null) || (cursor.getCount() == 0)){
-            throw new Resources.NotFoundException("Category not found");
-        }
-        cursor.moveToFirst();
-        Category category = categoryFromCursor(cursor);
-        cursor.close();
-        return category;
-    }
-
-    public Cursor loadAllCategories() {
-        Uri uri = CategoryEntry.CATEGORY_URI;
-        return getRecords(uri, CategoryEntry.SUMMARY_PROJECTION, null, null, null);
-    }
-
-    public void recordCompletedTask(long id) {
-        final ContentValues values = new ContentValues();
-        values.put(TasksEntry.COLUMN_COMPLETED, 1);
-        resolver.update(TasksEntry.TASK_URI, values, TasksEntry._ID + " = ?",
-                new String[]{ Long.toString(id) });
-    }
-
-    public void deleteTask(long id) {
-        resolver.delete(TasksEntry.TASK_URI, TasksEntry._ID + " = ?",
-                new String[] { Long.toString(id) });
-    }
-
-    public void deleteCategory(long id) {
-        resolver.delete(CategoryEntry.CATEGORY_URI, CategoryEntry._ID + " = ?",
-                new String[] { Long.toString(id) });
-    }
+    void deleteCategory(long id);
 }
---
+++ app/src/main/java/us/bridgeses/minder_tasks/storage/PersistenceHelperImpl.java
@@ -0,0 +1,172 @@
+package us.bridgeses.minder_tasks.storage;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
+
+import us.bridgeses.minder_tasks.models.Category;
+import us.bridgeses.minder_tasks.models.Task;
+
+/**
+ * Created by Tony on 4/16/2016.
+ *
+ * Helper for persisting and retrieving tasks and categories
+ */
+public class PersistenceHelperImpl implements TasksContract, PersistenceHelper {
+    private final ContentResolver resolver;
+
+    public PersistenceHelperImpl(Context context) {
+        resolver = context.getContentResolver();
+    }
+
+    @Override
+    public Cursor getRecords(Uri uri, String[] projection, String selection,
+                             String[] selectionArgs, String orderBy) {
+        return resolver.query(uri, projection, selection, selectionArgs, orderBy);
+    }
+
+    /**
+     * Convert a Cursor row to a Task
+     * @param cursor the cursor must already be set to the correct row
+     */
+    public Task taskFromCursor(Cursor cursor) {
+        final long id =
+                cursor.getLong(cursor.getColumnIndex(TasksEntry._ID));
+        final Category category =
+                loadCategory(cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_CATEGORY)));
+        final String name =
+                cursor.getString(cursor.getColumnIndex(TasksEntry.COLUMN_NAME));
+        final long created_time =
+                cursor.getLong(cursor.getColumnIndex(TasksEntry.COLUMN_CREATION_TIME));
+        final int duration =
+                cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_DURATION));
+        final long due_time =
+                cursor.getLong(cursor.getColumnIndex(TasksEntry.COLUMN_DUE_TIME));
+        final boolean completed =
+                cursor.getInt(cursor.getColumnIndex(TasksEntry.COLUMN_COMPLETED)) == 1;
+        return new Task.Builder(name)
+                .setId(id)
+                .setCategory(category)
+                .setCreationTime(created_time)
+                .setDueTime(due_time)
+                .setDuration(duration)
+                .setCompleted(completed)
+                .build();
+    }
+
+    public Category categoryFromCursor(Cursor cursor) {
+        final Long id =
+                cursor.getLong(cursor.getColumnIndex(CategoryEntry._ID));
+        final String name =
+                cursor.getString(cursor.getColumnIndex(CategoryEntry.COLUMN_NAME));
+        final int color =
+                cursor.getInt(cursor.getColumnIndex(CategoryEntry.COLUMN_COLOR));
+        return new Category(id, name, color);
+    }
+
+    public ContentValues cvFromTask(Task task) {
+        final ContentValues values = new ContentValues();
+        if (task.getId() >= 0) {
+            values.put(TasksEntry._ID, task.getId());
+        }
+        values.put(TasksEntry.COLUMN_NAME, task.getName());
+        if (task.getCategory() != null) {
+            values.put(TasksEntry.COLUMN_CATEGORY, task.getCategory().getId());
+        }
+        values.put(TasksEntry.COLUMN_CREATION_TIME, task.getCreationTime());
+        values.put(TasksEntry.COLUMN_DURATION, task.getDuration());
+        values.put(TasksEntry.COLUMN_DUE_TIME, task.getDueTime());
+        values.put(TasksEntry.COLUMN_COMPLETED, task.isCompleted());
+        return values;
+    }
+
+    public ContentValues cvFromCategory(Category category) {
+        final ContentValues values = new ContentValues();
+        if (category.getId() >= 0) {
+            values.put(CategoryEntry._ID, category.getId());
+        }
+        values.put(CategoryEntry.COLUMN_NAME, category.getName());
+        values.put(CategoryEntry.COLUMN_COLOR, category.getColor());
+        return values;
+    }
+
+    @Override
+    public long saveTask(Task task) {
+        final ContentValues values = cvFromTask(task);
+        Uri uri = resolver.insert(TasksEntry.TASK_URI, values);
+        if (uri == null) {
+            throw new Resources.NotFoundException("Failed to insert task");
+        }
+        return Long.parseLong(uri.getLastPathSegment());
+    }
+
+    @Override
+    public long saveCategory(Category category) {
+        final ContentValues values = cvFromCategory(category);
+        Uri uri = resolver.insert(CategoryEntry.CATEGORY_URI, values);
+        if (uri == null) {
+            throw new Resources.NotFoundException("Failed to insert category");
+        }
+        return Long.parseLong(uri.getLastPathSegment());
+    }
+
+    @Override
+    public Task loadTask(long id) throws Resources.NotFoundException {
+        Uri uri = TasksEntry.TASK_URI.buildUpon().appendPath(Long.toString(id)).build();
+        final Cursor cursor = getRecords(uri, null, null, null, null);
+        if ((cursor == null) || (cursor.getCount() == 0)){
+            throw new Resources.NotFoundException("Task not found");
+        }
+        cursor.moveToFirst();
+        Task task = taskFromCursor(cursor);
+        cursor.close();
+        return task;
+    }
+
+    private Cursor loadAllTasks() {
+        Uri uri = TasksEntry.TASK_URI;
+        return resolver.query(uri, TasksEntry.SUMMARY_PROJECTION, null, null, null);
+    }
+
+    @Override
+    public Category loadCategory(long id) throws Resources.NotFoundException {
+        Uri uri = CategoryEntry.CATEGORY_URI.buildUpon().appendPath(Long.toString(id)).build();
+        final Cursor cursor = getRecords(uri, null, null, null, null);
+        if ((cursor == null) || (cursor.getCount() == 0)){
+            throw new Resources.NotFoundException("Category not found");
+        }
+        cursor.moveToFirst();
+        Category category = categoryFromCursor(cursor);
+        cursor.close();
+        return category;
+    }
+
+    @Override
+    public Cursor loadAllCategories() {
+        Uri uri = CategoryEntry.CATEGORY_URI;
+        return getRecords(uri, CategoryEntry.SUMMARY_PROJECTION, null, null, null);
+    }
+
+    @Override
+    public void recordCompletedTask(long id) {
+        final ContentValues values = new ContentValues();
+        values.put(TasksEntry.COLUMN_COMPLETED, 1);
+        resolver.update(TasksEntry.TASK_URI, values, TasksEntry._ID + " = ?",
+                new String[]{ Long.toString(id) });
+    }
+
+    @Override
+    public void deleteTask(long id) {
+        resolver.delete(TasksEntry.TASK_URI, TasksEntry._ID + " = ?",
+                new String[] { Long.toString(id) });
+    }
+
+    @Override
+    public void deleteCategory(long id) {
+        resolver.delete(CategoryEntry.CATEGORY_URI, CategoryEntry._ID + " = ?",
+                new String[] { Long.toString(id) });
+    }
+}
